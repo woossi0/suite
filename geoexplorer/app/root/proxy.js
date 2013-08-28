e5d692a8c046443bc971226a1c0a11da30199d27
ahocevar <andreas.hocevar@gmail.com>
2013-08-28 14:06:04 +0200

    Strip Transfer-Encoding header
    ringo runs its own Transfer-Encoding, and we do not want to combine a chunked Transfer-Encoding with a Content-Length header.

--- geoexplorer/app/root/proxy.js
+++ geoexplorer/app/root/proxy.js
@@ -126,6 +126,8 @@ function proxyPass(config) {
     }
     exchange.wait();
     var headers = new Headers(objects.clone(exchange.headers));
+    // strip out transfer-encoding header because we run our own
+    headers.unset("Transfer-Encoding");
     if (!config.allowAuth) {
         // strip out authorization and cookie headers
         headers.unset("WWW-Authenticate");
