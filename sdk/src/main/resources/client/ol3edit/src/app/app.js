8258f6cabf3760193ba2a4d31949f915f7d8d228
Eva Shon <eshon@boundlessgeo.com>
2014-06-09 18:04:38 -0400

    Some suggestions for ol3edit template

--- sdk/src/main/resources/client/ol3edit/src/app/app.js
+++ sdk/src/main/resources/client/ol3edit/src/app/app.js
@@ -147,3 +147,35 @@ var deleteFeature = function() {
 var drawFeature = function() {
   transaction.activateInsert();
 };
+
+// on body resize update the map size
+var resizeMap = function() {
+  map.updateSize();
+}
+
+// add layers control collapse button
+// perhaps this should be moved into LayersControl.js?
+// It assumes Bootstrap however.
+var addLayersControlBtn = function() {
+  var layersControl =  $("#map .layers-control");
+
+  var btnString = '<button type="button" data-toggle="dropdown" class="layers-control-btn btn btn-default btn-sm"><i class="glyphicon glyphicon-minus"></button>';
+  var $btn = $($.parseHTML(btnString));
+  $btn.css('bottom', layersControl.height()-2);
+  $('#map').append($btn);
+
+  $btn.click(function () {
+    var iconDiv = $('.layers-control-btn > i.glyphicon');
+
+    if (iconDiv.hasClass('glyphicon-minus')) {
+      iconDiv.removeClass('glyphicon-minus')
+              .addClass('glyphicon-plus');
+    } else {
+      iconDiv.removeClass('glyphicon-plus')
+              .addClass('glyphicon-minus');
+    }
+    layersControl.toggle();
+  });
+
+}
+addLayersControlBtn();
